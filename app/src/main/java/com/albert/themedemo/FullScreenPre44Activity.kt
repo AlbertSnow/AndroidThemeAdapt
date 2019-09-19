46d3e8f681a259175b671a9eb4dedc91eeede248
ZhaoJiaLiang <zhaojialiang@sogou-inc.com>
2019-09-19 14:48:35 +0800

    build sketch for next step to test

---
+++ app/src/main/java/com/albert/themedemo/FullScreenPre44Activity.kt
@@ -0,0 +1,12 @@
+package com.albert.themedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullScreenPre44Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_pre44)
+    }
+}
---
+++ app/src/main/java/com/albert/themedemo/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.albert.themedemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import kotlin.reflect.KClass
+import java.lang.Class as Class
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        val listView : ListView = findViewById(R.id.main_jump_list_view)
+
+        listView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+            arrayOf(
+                "FullScreen Before 4.4 - WindowManager",
+                "FullScreen Before 4.4 - UI flag",
+                "FullScreen From 4.4 - Immersive",
+                ""
+            )
+        )
+
+        listView.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            val intent = Intent()
+            when (position) {
+                1 -> {
+                    intent.setClass(this, FullScreenPre44Activity::class.java)
+                }
+                2 -> {
+                    intent.setClass(this, FullScreenPre44UIFlagActivity::class.java)
+                }
+                3 -> {
+                    intent.setClass(this, FullScreenFrom44Activity::class.java)
+                }
+            }
+
+            startActivity(intent)
+        }
+
+
+    }
+}
